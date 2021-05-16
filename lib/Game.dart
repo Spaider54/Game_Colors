44fcb2615c1f4779dfbab5e3ae321ffa6b1ed784
Spaider54 <walidmenghour@hotmail.com>
2021-05-17 00:18:36 +0100

    Create a Game dart file

---
+++ lib/Game.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class Game extends StatefulWidget {
+  Game({Key key}) : super(key: key);
+
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+
+
+    )
+  }
+}
